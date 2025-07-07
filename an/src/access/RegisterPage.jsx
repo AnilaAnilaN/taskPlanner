93255a81b7f2bc5caf5f92eb2db7179dac761d51
Anila Nawaz <anilanawaz531@gmail.com>
2025-07-07 21:22:13 +0500

    Update RegisterPage.jsx

--- an/src/access/RegisterPage.jsx
+++ an/src/access/RegisterPage.jsx
@@ -23,7 +23,7 @@ const RegisterPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/register', formData);
+      await axios.post('api/register', formData);
       alert('Registration successful! Please log in using your credentials.');
       navigate('/login');
     } catch (error) {
