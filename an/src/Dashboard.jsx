5e856ed9680f48da4a5ecdc77b420e46991d9e55
Anila Nawaz <anilanawaz531@gmail.com>
2025-07-08 12:34:43 +0500

    Update Dashboard.jsx

--- an/src/Dashboard.jsx
+++ an/src/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-to-dom';
+import { Link } from 'react-router-dom';
 import { getTasks, getStudySessions } from './api';
 import Sidebar from './Sidebar';
 import Header from './Header';
