ed0f22735b349a2c49176a3e33250fa3134aa7ee
Anila Nawaz <anilanawaz531@gmail.com>
2025-07-08 09:07:54 +0500

    Update LoginPage.jsx

--- an/src/access/LoginPage.jsx
+++ an/src/access/LoginPage.jsx
@@ -32,7 +32,7 @@ const LoginPage = ({ onLogin }) => {
       setTimeout(() => {
         onLogin();
         navigate('/dashboard');
-      }, 2000); // Redirect after 2 seconds
+      }, 2000);
     } catch (error) {
       console.error('Login error:', error);
       setLoading(false);
